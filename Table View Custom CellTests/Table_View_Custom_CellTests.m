0a7d419a7a4edbb05defeaa4e30d48de28aa2a40
Muhammed <MacBook@Muhammed-MacBook-Pro.local>
2014-05-19 19:58:26 +0300

    Initial Commit

---
+++ Table View Custom CellTests/Table_View_Custom_CellTests.m
@@ -0,0 +1,34 @@
+//
+//  Table_View_Custom_CellTests.m
+//  Table View Custom CellTests
+//
+//  Created by Muhammed on 19.05.2014.
+//  Copyright (c) 2014 Muhammed. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Table_View_Custom_CellTests : XCTestCase
+
+@end
+
+@implementation Table_View_Custom_CellTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
